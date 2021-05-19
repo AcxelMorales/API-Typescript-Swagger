ebbcbe39cbb97cf620c9cc2c37eec7bc5da9fac6
Acxel Morales <acxelmorales97@gmail.com>
2021-05-19 18:53:18 -0500

    Add new config proyect

---
+++ src/controllers/task.controller.ts
@@ -0,0 +1,25 @@
+import { Handler, Request, Response } from 'express';
+
+export const getTasks: Handler = (req: Request, res: Response) => {
+  res.json();
+};
+
+export const getCountTasks: Handler = (req: Request, res: Response) => {
+  res.json();
+};
+
+export const getTaskById: Handler = (req: Request, res: Response) => {
+  res.json();
+};
+
+export const createTask: Handler = (req: Request, res: Response) => {
+  res.json();
+};
+
+export const deleteTask: Handler = (req: Request, res: Response) => {
+  res.json();
+};
+
+export const updateTask: Handler = (req: Request, res: Response) => {
+  res.json();
+};
